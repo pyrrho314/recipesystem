f65e3b69b47495cbb841a44ec569f6f9bab21704
Craig Allen <craig.b.allen@gmail.com>
2015-07-06 23:54:08 +0000

    quick fix

--- trunk/astrodata/adutils/dwutil/daemon_process.py
+++ trunk/astrodata/adutils/dwutil/daemon_process.py
@@ -13,8 +13,9 @@ ingest_sources = Lookups.compose_multi_table(
 print "d_p8:", ks.dict2pretty("i_s", ingest_sources)
 
 watch_procs = {}
-for source in ingest_sources:
-    watchproc = ShelfWatch(source)
-    watch_procs[watchproc["key_path"]] = watchproc
+if ingest_sources:
+    for source in ingest_sources:
+        watchproc = ShelfWatch(source)
+        watch_procs[watchproc["key_path"]] = watchproc
 
 
